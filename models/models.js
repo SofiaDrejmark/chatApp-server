feb4698cc21e30081f9f75c8051a0bf25d5ba3f6
SofiaDrejmark <sofiadrejmark@gmail.com>
2022-08-24 20:50:07 +0200

    changed some stuff

--- models/models.js
+++ models/models.js
@@ -160,121 +160,67 @@ module.exports = {
 }; */
 
 const db = require("../config/db");
-const res = require("express/lib/response");
 
-// Get all Rooms
-function getRooms() {
+async function getRooms () {
   const sql = `SELECT * FROM rooms`;
-    return db.query(sql, (error, rooms) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(200);
-      return rooms;
-    });
+  const result = await db.query(sql)
+  return result.rows;
 }
 
 // Get all Users
-function getUsers() {
+
+
+
+async function getUsers() {
   const sql = `SELECT * FROM users`;
-    return db.query(sql, (error, users) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(200);
-      return users;
-    });
+    const result = await db.query(sql) 
+    return result.rows; 
   
 }
 
 // Get one Room
-function getRoom() {
+async function getRoom() {
   const sql = `SELECT room FROM rooms`;
-    return db.query(sql, (error, room) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(201);
-      return room;
-    });
+    const result = await db.query(sql)
+   return result.rows;
 }
 // Add Rooms
-function addRoom(room) {
+async function addRoom(room) {
   const sql = `INSERT INTO rooms (room) VALUES ($1)`;
-    return db.query(sql, [room], (error) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(200);
-      return room;
-    });
+    const result = await db.query(sql, [room])
+   return result.rows;
 }
 // Add User
-function addUser(username) {
+async function addUser(username) {
   const sql = `INSERT INTO users (username) VALUES ($1)`;
-    return db.query(sql, [username], (error) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(200);
-      return username;
-    });
+    const result = await db.query(sql, [username])
+    return result.rows;
 }
 
 // Delete Room
-function deleteRoom(room) {
+async function deleteRoom(room) {
   const sql = `DELETE FROM rooms WHERE room = $1`;
-    return db.query(sql, room, (error) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      console.log("Room deleted");
-      res.status(204);
-      return room;
-    });
+    const result = await db.query(sql, [room])
+      return result.rows;
 }
 
 //Messages
-function getMessages(room) {
+async function getMessages(room) {
   const sql = `SELECT * FROM messages WHERE room = $1`;
-   return db.query(sql, room, (error, rows) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(201);
-      return rows;
-    });
+   const result = await db.query(sql, [room])
+   return result.rows;
 }
 
-function deleteMessages(room) {
+async function deleteMessages(room) {
   const sql = `DELETE FROM messages WHERE room =$1`;
-    return db.query(sql, room, (error) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(201);
-      return;
-    });
+    const result = await db.query(sql, [room])
+    return result.rows;
 }
 
-function addMessage(data) {
+async function addMessage(data) {
   const sql = `INSERT INTO messages (message, room, username, time) VALUES ($1,$2,$3,$4)`;
-    return db.query(sql, [ data.message, data.room, data.username, data.time], (error, rows) => {
-      if (error) {
-        console.error(error.message);
-        res.status(400);
-      }
-      res.status(201);
-      return rows;
-    });
+    const result = await db.query(sql, [ data.message, data.room, data.username, data.time])
+    return result.rows;
 }
 
 module.exports = {
